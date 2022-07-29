2368cb28e2a89016c51928df233c3ef5686e5e69
Sergey <92508690+SN737@users.noreply.github.com>
2022-07-30 02:02:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let addMessage = document.querySelector('.message');
 let addButton = document.querySelector('.add');
-let  todo = document.querySelector('.todo')
-const disableButton = () => {document.querySelector('.add').disabled = true;}
+let  todo = document.querySelector('.todo');
+
+
+
+const disableButton = () => {document.querySelector('.add').disabled = true;};
 disableButton();
 
 addMessage.addEventListener('input', () => {
     if(addMessage.value.length < 1){
-        disableButton
+        disableButton();
     } else{
     document.querySelector('.add').disabled = false;}
-   
-
-})
+  });
 
 
 
@@ -20,28 +21,29 @@ addMessage.addEventListener('input', () => {
 let todoList = []; 
 
 if (localStorage.getItem('todo')){
-    todoList = JSON.parse(localStorage.getItem('todo'))
+    todoList = JSON.parse(localStorage.getItem('todo'));
     displayMessages();
-};
+    
+}
 
-let delbtn = document.querySelector('.btndel');
-delbtn.addEventListener('click', (event) => {
-    let valueLabel = todo.querySelector('[for='+event.target.getAttribute('id') + ']').innerHTML
-    alert (valueLabel)
-})
+
+
+
+  
 
  todo.addEventListener('change', (event) => {
-    let valueLabel = todo.querySelector('[for='+event.target.getAttribute('id') + ']').innerHTML
+    let valueLabel = todo.querySelector('[for='+event.target.getAttribute('id') + ']').innerHTML;
     todoList.forEach ((item) => {
         if (item.todo === valueLabel){
-            item.checked = !item.checked
-            localStorage.setItem('todo', JSON.stringify(todoList))
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList));
                 }
-    })
-    console.log(todoList);
+    });
+    
+    displayMessages();
 
 
- })
+ });
 
  addButton.addEventListener('click', () => {
     if (addMessage.value.length > 0) {
@@ -54,14 +56,16 @@ delbtn.addEventListener('click', (event) => {
     let newTodo = {
         todo: addMessage.value,
         checked: false,
-        importan: false
+        importan: false, 
+        idn: ''
     };
-    todoList.push(newTodo)
+    todoList.push(newTodo);
+    
     displayMessages();
     localStorage.setItem('todo', JSON.stringify(todoList));
-    document.querySelector('.message').value = ''
+    document.querySelector('.message').value = '';
 
- })
+ });
 
  function displayMessages() {
     
@@ -71,30 +75,43 @@ delbtn.addEventListener('click', (event) => {
         displayMessage += `
         <li>
          <input type ='checkbox' id='item_${i}' ${item.checked ? 'true' : 'false'}>
-         <label for = 'item_${i}'>${item.todo}</label>
-         <button class ='btndel'>удалить</button>
+         <label id = ${i} for = 'item_${i}'>${item.todo}</label>
+         <button id = '${i}' class = 'btndel'>удалить</button>
         </li>
          `;
         todo.innerHTML = displayMessage;
+        item.idn = i;
+       
 
-    })
+    });
  }
 
- function deleteMessages(){
-
-
+ function deleteMessages(idn){
+      
 
     if (confirm('Точно удалить?')){
-        todoArray = JSON.parse(localStorage.getItem(todo));
-        const newList = todoArray.filter(obj => obj.id != item.id);
-        localStorage.setItem(key, JSON.stringify(newList));
-            
+        todoList = JSON.parse(localStorage.getItem('todo'));
+        console.log (idn);
+        const newList = todoList.filter(obj => obj.idn != idn);
+        localStorage.setItem('todo', JSON.stringify(newList));
+        todoList = JSON.parse(localStorage.getItem('todo'));
+       displayMessages();
     
-    item.remove();}
-
+    //item.remove();}
+    }
  }
 
  todo.addEventListener('contextmenu', (e) => {
    e.preventDefault();
+ });
 
- })
+
+ // if (localStorage.getItem('todo')) {
+
+todo.addEventListener('click', (e) => {
+    if (e.target.className != 'btndel') {
+        return;}
+        let idn = e.target.getAttribute ('id');
+    deleteMessages(idn);
+});
+// }
